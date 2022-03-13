30e51e9cb937bfbde1a95523c1c3199d5432192e
Erik Jones <92000838+Jonzee1914@users.noreply.github.com>
2022-03-13 09:22:28 -0400

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
       
       <div className="row justify-content-center">
         <div className="col-10" id="about-section">
-          <img class="mb-5" id="avatar" src="" alt="Erik Jones"/>
+          <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/92000838?s=400&u=874c985327b9343bc92f2681a05f3906a8c4d5f2&v=4" alt="Erik Jones"/>
         
         <p> I currently am a Member Experience Specialist, for a credit union in Central Florida. The ever changing face of the financial industry has motivated
             me to expand my horizons and embark on a path to being a full stack web developer. This portfolio and my bio is a little light right now, but check back in a few months
